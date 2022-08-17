f11d1db1284919d4585dbb050a5cb2e77a45b5d5
Joel Abril Madrigal <90817452+joelAbril@users.noreply.github.com>
2022-08-17 12:58:46 -0600

    Update autenticar_usuario.php

--- php/autenticar_usuario.php
+++ php/autenticar_usuario.php
@@ -8,16 +8,39 @@ $connection = mysqli_connect("localhost","root","","clinicadentalbd");
 $consulta="SELECT*FROM usuarios where usuario='$usuario' and contrasena='$contrasena'";
 $resultado = mysqli_query($connection,$consulta);
 $filas=mysqli_fetch_array($resultado);
+if(isset($_COOKIE["block".$usuario])){
+            ?>
+            <?php
+            include("login.php");
+            ?>
+            <h1 class="Bad">Usuario Bloqueado por 1 minuto</h1>
+            <?php
+    
+}else{
 
-if($filas){
-    header("location:paginaPrincipal.php");
-} else{
-        ?>
-        <?php
-        include("login.php");
-        ?>
-        <h1 class="Bad">No existe usuario o contrasenna</h1>
-        <?php
+    if($filas){
+        header("location:paginaPrincipal.php");
+    } else{
+            ?>
+            <?php
+            include("login.php");
+            ?>
+            <h1 class="Bad">No existe usuario o contrasenna</h1>
+            <?php
+
+            if(isset($_COOKIE["$usuario"])){
+                $cont = $_COOKIE["$usuario"];
+                $cont++;
+                setcookie($usuario, $cont,time()+120);
+                if($cont >=3){
+                    setcookie("block".$usuario, $cont,time()+60);
+
+                }
+
+            }else{
+                setcookie($usuario,1,time()+120);
+            }
+        }
     }
     mysqli_free_result($resultado);
     mysqli_close($connection);
