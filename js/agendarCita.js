c5c6d6914761ecf55602367bbfa9b7997e4d2376
Ellis Mayorga <ellismayorga@Ellis-MacBook-Air.local>
2022-08-20 18:25:00 -0600

    modificar agenda

--- js/agendarCita.js
+++ js/agendarCita.js
@@ -5,11 +5,9 @@ var calendar = "";
 
   $(function () {
       $('#datepicker').datepicker({ 
-          beforeShowDay: $.datepicker.noWeekends, 
-
           onSelect : function (date, datepicker){
               calendar = date;
-              console.log(calendar);
+             //console.log(calendar);
           }
       });
   });
